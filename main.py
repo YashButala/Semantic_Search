189eccfdc7f639673338ec5eaf75a8b3f18630e3
anunay777 <45771163+anunay777@users.noreply.github.com>
2019-11-15 02:24:53 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,49 +1,110 @@
-from flask import Flask, request
-import requests
-from query import do_query
+from nltk.tokenize import word_tokenize
+import nltk
+from nltk.corpus import stopwords
+import sparql
 
-app = Flask(__name__)
+search = input("Search:")
+processedWords = []
+words = nltk.word_tokenize(search)
+sTwords = stopwords.words('english')
 
-@app.route("/", methods=["GET", "POST"])
-def adder_page():
-    errors = ""
-    if request.method == "POST":
-        question = None
-        try:
-            question = request.get_data()
-            question=str(question[6:-3])
-            question=question.replace("+"," ")
+print(words)
+#Removing stopwords
+flag=0
+for word in words:
+    if word not in sTwords:
+        if(flag==0):
+            flag=1
+        else:
+            flag=2
+        processedWords.append(word)
+    else:
+        if(flag==2):
+            processedWords.append(word)
 
-        except:
-            errors += "<p>{!r} is not a query_string.</p>\n"
+print(processedWords)
+tagged_words = nltk.pos_tag(processedWords)
+print('Searching...')
 
-        if question is not None :
-       # 	ques = question.decode('utf-8')
-        	print(question[2:-1])
-        	result = do_query(question[2:-1])
-        	print(result)
-        	return '''
-		    	<html>
-		    		<body>
-		                <p>The result is {result}</p>
-		                <p><a href="/">Click here to calculate again</a>
-		            </body>
-		        </html>
-		    '''.format(result=result)
+#Case where was born
+# if (tagged_words[0][1] == 'WRB' and tagged_words[0][0] == 'Where' and tagged_words[1][1] == 'VBN'):
+#     term = ''
+#     i = 0
+#     for word in tagged_words:
+#         if (word[1] == 'NNP'):
+#             if (i == 0):
+#                 term += word[0]
+#             else:
+#                 term += ' ' + word[0]
 
-    return '''
-        <html>
-            <body>
-                {errors}
-                <p>Enter your query:</p>
-                <form method="post" action=".">
-                    <p><input name="Query" /></p>
-                    <p><input type="submit" value="Search" /></p>
-                </form>
-            </body>
-        </html>
-    '''.format(errors=errors)
-if __name__ == "__main__":
-#    app.secret_key = 'super secret key'
-#    app.config['SESSION_TYPE'] = 'filesystem'
-    app.run(debug=True)
+#             i += 1
+    
+#     sparql.whereWasBorn(term)
+
+#Case who
+# if (tagged_words[0][1] == 'WP'):
+#     name = ''
+#     i = 0
+#     for word in tagged_words:
+#         if (word[1] == 'NNP'):
+#             if (i == 0):
+#                 name += word[0]
+#             else:
+#                 name += ' ' + word[0]
+        
+#             i += 1
+#     sparql.whoIs(name)
+
+#Case where
+# if (tagged_words[0][1] == 'WRB'):
+#     place = ''
+#     i = 0
+#     for word in tagged_words:
+#         if (word[1] != 'WRB' and word[1] != '.'):
+#             if (i == 0):
+#                 place += word[0]
+#             else:
+#                 place += ' ' + word[0]
+
+#             i += 1
+            
+#     sparql.whereIs(place)
+
+#Case what
+if (tagged_words[0][1] == 'WP' and tagged_words[0][0] == 'What'):
+    term = ''
+    #term+=tagged_words[2][0]
+    attribute=''
+    #attribute+=tagged_words[1][0]
+    i = 0
+    f = 0
+    for word in tagged_words:
+        if(f):
+            term+=' '+word[0]
+        else:
+            if(word[0]=='of'):
+                f=1;
+            elif(word[0]!='What'):
+                attribute+=' '+word[0]
+    attribute=attribute[1:]
+    term=term[1:]
+    print(tagged_words)
+    print(attribute)
+    print(term)
+    print(attribute)
+    sparql.whatIs(term,attribute)
+
+# # case how to cook
+# if (tagged_words[0][1] == 'WRB' and tagged_words[0][0] == 'How'):
+#     term = ''
+#     i = 0
+#     for word in tagged_words:
+#         if (word[1] == 'NNP'):
+#             if (i == 0):
+#                 term += word[0]
+#             else:
+#                 term += ' ' + word[0]
+
+#             i += 1
+    
+#     sparql.howToCook(term)
--- sparql.py
+++ sparql.py
@@ -1,160 +1,153 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 
-
-def whoIs(person):
-  ret = ""
-  sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-  sql = """ 
-      PREFIX  dbpedia-owl:  <http://dbpedia.org/ontology/>
-      PREFIX dbpedia: <http://dbpedia.org/resource>
-      PREFIX dbpprop: <http://dbpedia.org/property>
-
-     SELECT DISTINCT ?person ?comment ?label
-      WHERE {
-        ?person rdf:type dbpedia-owl:Person.
-        ?person rdfs:comment ?comment.  
-        ?person rdfs:label ?label
-        FILTER regex(?label, "^%s", "i")
-        FILTER (LANG(?comment) = 'pt') 
-      }
-      LIMIT 1
-      """ % ''.join((person))
+import json
+# def whoIs(person):
+#   sparql = SPARQLWrapper("http://query.wikidata.org/sparql")
+#   sql = """ 
+ 
+#       """# % ''.join((person))
   
-  sparql.setQuery(sql)
+#   sparql.setQuery(sql)
   
 
-  sparql.setReturnFormat(JSON)
-  results = sparql.query().convert()
+#   sparql.setReturnFormat(JSON)
+#   results = sparql.query().convert()
 
-  for result in results["results"]["bindings"]:
-      ret+=(result["comment"]["value"])
-  return ret
-     
+#   for result in results["results"]["bindings"]:
+#       print(result["comment"]["value"])
 
 
-def whereIs(location):
-  ret = ""
-  sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-  sql = """ 
-      PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
-      PREFIX dbo: <http://dbpedia.org/ontology/>
-      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-      PREFIX dbp: <http://dbpedia.org/property/>
+# def whereIs(location):
+#   sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+#   sql = """ 
+#       PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
+#       PREFIX dbo: <http://dbpedia.org/ontology/>
+#       PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+#       PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+#       PREFIX dbp: <http://dbpedia.org/property/>
 
-      SELECT *
-      WHERE  { 
-        ?location rdf:type dbo:Location.
-        ?location dbo:location ?country.
-        ?location rdfs:label ?label.
-        OPTIONAL {
-          ?country dbp:coordinatesType ?city.
-        }
-        ?country rdfs:label ?countryLabel.
+#       SELECT *
+#       WHERE  { 
+#         ?location rdf:type dbo:Location.
+#         ?location dbo:location ?country.
+#         ?location rdfs:label ?label.
+#         OPTIONAL {
+#           ?country dbp:coordinatesType ?city.
+#         }
+#         ?country rdfs:label ?countryLabel.
 
-        FILTER regex(?label, "^%s", "i").
+#         FILTER regex(?label, "^%s", "i").
        
         
 
-      }
-      LIMIT 1
-      """ % ''.join((location))
+#       }
+#       LIMIT 1
+#       """ #% ''#.join((location))
 
-  sparql.setQuery(sql)
+#   sparql.setQuery(sql)
   
 
-  sparql.setReturnFormat(JSON)
-  results = sparql.query().convert()
+#   sparql.setReturnFormat(JSON)
+#   results = sparql.query().convert()
 
-  for result in results["results"]["bindings"]:
-      ret+=(result["countryLabel"]["value"])
-  return ret    
+#   for result in results["results"]["bindings"]:
+#       print(result["countryLabel"]["value"])
 
-def whatIs(term):
-  ret=""
-  sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+
+def whatIs(term,attribute):
+  sparql = SPARQLWrapper("http://query.wikidata.org/sparql")
+  mydict={attribute,term}
+ # mydict.append(attribute)
+ # mydict.append(term)
   sql = """ 
-  PREFIX w3-owl: <http://www.w3.org/2002/07/owl#>
-  SELECT ?thing, ?comment, ?label
-    WHERE {
-      ?thing rdf:type w3-owl:Thing.
-      ?thing rdfs:comment ?comment.
-      ?thing rdfs:label ?label.
-      FILTER regex(?label, "^%s", "i").
-      FILTER (lang(?comment) = 'pt')
-    }
-  LIMIT 1
-  """ % ''.join((term))
+SELECT DISTINCT  ?res
+WHERE
+{
+  ?property wdt:P31/wdt:P279* wd:Q18616576;
+           rdfs:label "%s"@en;
+            wikibase:directClaim  ?wdt.
+  ?country wdt:P31 wd:Q3624078.
+   ?country  ?wdt ?res;
+           rdfs:label "%s"@en;
+           rdfs:label ?countryLabel.
+  FILTER (lang(?countryLabel) = "en")
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+}
 
+ORDER BY DESC(?res)
+LIMIT 10
+  """%(attribute,term)
+  print(term,attribute)
   sparql.setQuery(sql)
 
   sparql.setReturnFormat(JSON)
   results = sparql.query().convert()
-
   for result in results["results"]["bindings"]:
-      ret+= (result["comment"]["value"])
-  return ret    
-def howToCook(term):
-  ret=""
-  sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-  sql = """ 
-  PREFIX  dbpedia-owl:  <http://dbpedia.org/ontology/>
-  PREFIX dbpedia: <http://dbpedia.org/resource>
-  PREFIX dbpprop: <http://dbpedia.org/property>
-     SELECT *
-      WHERE {
-        ?dessert rdf:type dbpedia-owl:Food.
-        ?dessert dbpedia-owl:ingredientName ?ing.
-        ?dessert dbpedia-owl:servingTemperature ?servingTemp.
-        ?dessert rdfs:comment ?comment.
-        ?dessert rdfs:label ?label.
-        FILTER regex(?label, "^%s", "i")
+      print(result["res"]["value"],end="\t")
+ #     print(result["population"]["value"],end="\n")
+  #for result in results["results"]["population"]:
+ #y=json.loads(results)
+  #print(results)
+  #jstring = json.dumps(results)
+  #print(jstring)
+
+# def howToCook(term):
+#   sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+#   sql = """ 
+#   PREFIX  dbpedia-owl:  <http://dbpedia.org/ontology/>
+#   PREFIX dbpedia: <http://dbpedia.org/resource>
+#   PREFIX dbpprop: <http://dbpedia.org/property>
+#      SELECT *
+#       WHERE {
+#         ?dessert rdf:type dbpedia-owl:Food.
+#         ?dessert dbpedia-owl:ingredientName ?ing.
+#         ?dessert dbpedia-owl:servingTemperature ?servingTemp.
+#         ?dessert rdfs:comment ?comment.
+#         ?dessert rdfs:label ?label.
+#         FILTER regex(?label, "^%s", "i")
         
-        FILTER (LANG(?comment) = 'pt') 
+#         FILTER (LANG(?comment) = 'pt') 
 
-      }
-      LIMIT 1
-  """ % ''.join((term))
+#       }
+#       LIMIT 1
+#   """ #% ''.join((term))
 
-  sparql.setQuery(sql)
+#   sparql.setQuery(sql)
 
-  sparql.setReturnFormat(JSON)
-  results = sparql.query().convert()
+#   sparql.setReturnFormat(JSON)
+#   results = sparql.query().convert()
 
-  for result in results["results"]["bindings"]:
-      ret+= (result["comment"]["value"] + "\n")
-      ret+= ('Ingredientes: ' + result["ing"]["value"] + "\n")
-      ret+= ('Servir: ' + result["servingTemp"]["value"] + "\n")
+#   for result in results["results"]["bindings"]:
+#       print(result["comment"]["value"] + "\n")
+#       print('Ingredientes: ' + result["ing"]["value"] + "\n")
+#       print('Servir: ' + result["servingTemp"]["value"] + "\n")
 
-  return ret
-      
-def whereWasBorn(person):
-  ret=""
-  sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-  sql = """ 
-      PREFIX dbo: <http://dbpedia.org/ontology/>
-      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 
-      SELECT *
-      WHERE  { 
-        ?person rdf:type dbo:Person.
-        ?person rdfs:label ?label.
-        ?person dbo:birthPlace ?country.
-        ?country rdfs:label ?birthPlace.
-        FILTER regex(?label, "^%s", "i")
+# def whereWasBorn(person):
+#   sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+#   sql = """ 
+#       PREFIX dbo: <http://dbpedia.org/ontology/>
+#       PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+#       PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 
-      }
+#       SELECT *
+#       WHERE  { 
+#         ?person rdf:type dbo:Person.
+#         ?person rdfs:label ?label.
+#         ?person dbo:birthPlace ?country.
+#         ?country rdfs:label ?birthPlace.
+#         FILTER regex(?label, "^%s", "i")
 
-      LIMIT 1
-      """ % ''.join((person))
+#       }
+
+#       LIMIT 1
+#       """ #% ''.join((person))
   
-  sparql.setQuery(sql)
+#   sparql.setQuery(sql)
   
 
-  sparql.setReturnFormat(JSON)
-  results = sparql.query().convert()
+#   sparql.setReturnFormat(JSON)
+#   results = sparql.query().convert()
 
-  for result in results["results"]["bindings"]:
-      ret+=(result["birthPlace"]["value"])
-  return ret
+#   for result in results["results"]["bindings"]:
+#       print(result["birthPlace"]["value"])
